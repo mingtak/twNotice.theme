30d682021ecd9dbe3d56059eb48577b73df80cca
Andy Fang <andy@mingtak.com.tw>
2016-06-26 11:08:56 +0800

    add countdown to notice_view

--- src/twNotice/theme/viewlet/viewlets.py
+++ src/twNotice/theme/viewlet/viewlets.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
 from plone.app.layout.viewlets import common as base
+from plone import api
+from DateTime import DateTime
 
 
 class ContactUsInHeader(base.ViewletBase):
     """  """
+
+
+class CountDown(base.ViewletBase):
+    """  """
+
+    def get_deadline(self):
+        context = self.context
+        request = self.request
+
+        notice_view = api.content.get_view(
+            name='notice_view',
+            context=context,
+            request=request,
+        )
+
+        result = {}
+        try:
+            deadline = notice_view.get_deadline()
+            result['year'] = str(int(deadline.split('/')[0]) + 1911)
+            result['month'] = deadline.split('/')[1]
+            result['day'] = deadline.split('/')[2].split()[0]
+            result['hour'], result['minute'] = deadline.split()[1].split(':')[0:2]
+        except:
+            return None
+
+        return result
+
+
